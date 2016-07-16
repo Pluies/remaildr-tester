e305ff8519903f5ff617e95e31a5ba1e7580a337
Florent Delannoy <florent.delannoy@gmail.com>
2016-07-16 22:14:06 +0100

    Better handling of received probes

--- remaildrtester/probe/receiver.py
+++ remaildrtester/probe/receiver.py
@@ -1,6 +1,8 @@
 import arrow
 import email
 import imaplib
+import sys
+import traceback
 from django.conf import settings
 from website.models import RemotePoll
 
@@ -29,20 +31,28 @@ class Receiver:
             M.select('inbox')
             typ, data = M.search(None, 'ALL')
             for num in data[0].split():
-                typ, msg_data = M.fetch(num, '(RFC822)')
-                raw_msg = msg_data[0][1]
-                msg = email.message_from_string(raw_msg.decode('utf-8'))
-                subject = msg['subject']
-                if subject.startswith('Remaildr: Probe: '):
-                    probe_id = subject.replace('Remaildr: Probe: ', '')
-                    logger.info("Found probe #%s" % probe_id)
-                    try:
-                        probe = RemotePoll.objects.get(probe_id=probe_id)
-                        probe.status = RemotePoll.SUCCESS
-                        probe.completed_at = arrow.utcnow().datetime
-                        probe.save()
-                        M.store(num, '+FLAGS', '\\Deleted')
-                        M.expunge()
-                    except:
-                        logger.error("Probe %s not found in db - skipping" %
-                                     probe_id)
+                try:
+                    typ, msg_data = M.fetch(num, '(RFC822)')
+                    if not msg_data or not msg_data[0] or not msg_data[0][1]:
+                        return
+                    raw_msg = msg_data[0][1]
+                    msg = email.message_from_string(raw_msg.decode('utf-8'))
+                    subject = msg['subject']
+                    if subject.startswith('Remaildr: Probe: '):
+                        probe_id = subject.replace('Remaildr: Probe: ', '')
+                        logger.info("Found probe #%s" % probe_id)
+                        try:
+                            probe = RemotePoll.objects.get(probe_id=probe_id)
+                            probe.status = RemotePoll.SUCCESS
+                            probe.completed_at = arrow.utcnow().datetime
+                            probe.save()
+                        except:
+                            logger.error("Probe %s not found in db - skipping" %
+                                         probe_id)
+                except:
+                    e = sys.exc_info()[0]
+                    logger.error("Uncaught exception: %s" % e)
+                    traceback.print_exc(file=sys.stdout)
+                finally:
+                    M.store(num, '+FLAGS', '\\Deleted')
+                    M.expunge()
